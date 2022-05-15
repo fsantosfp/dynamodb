3d1543109058aa64823f952ce558299c0feeae17
Fabio Santos <fsantos.fps@gmail.com>
2022-05-15 20:29:08 -0300

    Start Project

---
+++ src/test/kotlin/com/learn/aws/dynamodb/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.learn.aws.dynamodb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
